01b4b19625e40511809c079514f2f1c79d434f27
Sadra <alizadeh.sadra.gh@gmail.com>
2024-05-28 20:38:29 +0300

    Editing Part 19 of Section 2

--- src/Siralama_Algoritmalar/StrandSort_2_19.java
+++ src/Siralama_Algoritmalar/StrandSort_2_19.java
@@ -6,49 +6,73 @@ import java.util.List;
 import java.util.Scanner;
 
 public class StrandSort_2_19 {
-    static void strandSort(List<Integer> input, List<Integer> output) {
-        if (input.isEmpty())
-            return;
+    public static List<Integer> mergeLists(List<Integer> list1, List<Integer> list2) {
+        List<Integer> result = new ArrayList<>();
+        while (!list1.isEmpty() && !list2.isEmpty()) {
+            if (list1.get(0) < list2.get(0)) {
+                result.add(list1.remove(0));
+            } else {
+                result.add(list2.remove(0));
+            }
+        }
+        result.addAll(list1);
+        result.addAll(list2);
+        return result;
+    }
 
-        List<Integer> sublist = new LinkedList<>();
-        sublist.add(input.get(0));
-        input.remove(0);
+    // Recursive function to perform strand sort
+    public static List<Integer> strandSort(List<Integer> inputList) {
+        // Base case: if the input list has 1 or fewer elements, it's already sorted
+        if (inputList.size() <= 1) {
+            return inputList;
+        }
 
-        for (int i = 0; i < input.size(); ) {
-            if (input.get(i) > sublist.get(sublist.size() - 1)) {
-                sublist.add(input.get(i));
-                input.remove(i);
+        // Initialize a sublist with the first element of the input list
+        List<Integer> sublist = new ArrayList<>();
+        sublist.add(inputList.remove(0));
+
+        int i = 0;
+        while (i < inputList.size()) {
+            // If the current element in the input list is greater than
+            // the last element in the sublist,
+            // add it to the sublist; otherwise, continue to the next element in the input list.
+            if (inputList.get(i) > sublist.get(sublist.size() - 1)) {
+                sublist.add(inputList.remove(i));
             } else {
                 i++;
             }
         }
 
-        output.addAll(sublist);
-        strandSort(input, output);
+        // The sortedSublist contains the sorted elements from the current sublist
+        List<Integer> sortedSublist = new ArrayList<>(sublist);
+
+        // Recursively sort the remaining part of the input list
+        List<Integer> remainingList = strandSort(inputList);
+
+        // Merge the sorted sublist and the sorted remainingList
+        return mergeLists(sortedSublist, remainingList);
     }
 
     public static void main() {
+        List<Integer> inputList = new ArrayList<>();
 
+        Scanner input = new Scanner(System.in);
 
-
-        Scanner input_user = new Scanner(System.in);
         System.out.println("Please enter the dimension of array: ");
-        int dimension = input_user.nextInt();
-        List<Integer> myList = new ArrayList<>();
+        int dimension = input.nextInt();
 
         System.out.println("Please enter the array elements: ");
         for (int i = 0; i < dimension; i++) {
             System.out.println("Please enter the " + (i + 1) + ". number: ");
-            myList.add(input_user.nextInt());
+            int number = input.nextInt();
+            inputList.add(number);
         }
 
 
-        List<Integer> input = new LinkedList<>(myList);
-        List<Integer> output = new LinkedList<>();
-        strandSort(input, output);
 
-        System.out.print("The result: ");
-        for (int x : output) {
+        List<Integer> outputList = strandSort(inputList);
+
+        for (int x : outputList) {
             System.out.print(x + " ");
         }
         System.out.println("\n");
